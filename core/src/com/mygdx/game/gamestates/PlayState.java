f5527da20084210b41b00dbcabfa4ca80467fff4
Carl Englund <englund92@gmail.com>
2015-09-22 15:55:35 +0200

    Boundaries imported from tmx map now works

--- core/src/com/mygdx/game/gamestates/PlayState.java
+++ core/src/com/mygdx/game/gamestates/PlayState.java
@@ -3,12 +3,19 @@ package com.mygdx.game.gamestates;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.managers.GameStateManager;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.mygdx.game.utils.TiledObjectUtil;
 
 
 /**
@@ -20,6 +27,13 @@ public class PlayState extends GameState {
     TiledMap tiledMap;
     TiledMapRenderer tiledMapRenderer;
     OrthographicCamera camera;
+    Texture texture;
+    SpriteBatch batch;
+
+    private World world;
+    private Box2DDebugRenderer b2dr;
+    private Body player;
+
 
 
 
@@ -35,23 +49,39 @@ public class PlayState extends GameState {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 512, 512);
         camera.update();
+        batch = new SpriteBatch();
+
+
+        world = new World(new Vector2(0, -9.8f), false);
+        b2dr = new Box2DDebugRenderer();
+        texture = new Texture("assets/splash.png");
+
+
+        player = createBox(100f, 200f, 16f, 16f, false);
+
+        TiledObjectUtil.parseTiledObjectLayer(world, tiledMap.getLayers().get("collision-layer").getObjects());
+
 
 
     }
 
     @Override
     public void update(float dt) {
-        handleInput();
+        //handleInput();
+        world.step(1 / 60f, 6, 2);
+        inputUpdate(dt);
+
     }
 
     @Override
     public void draw() {
+        update(Gdx.graphics.getDeltaTime());
+
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
-        playerObject.begin(ShapeRenderer.ShapeType.Filled);
-        playerObject.setColor(0, 1, 0, 1);
-        playerObject.rect(250, 250, 20, 20);
-        playerObject.end();
+        batch.begin();
+            batch.draw(texture, player.getPosition().x * 70 - (texture.getWidth() / 2), player.getPosition().y * 70 - (texture.getHeight() / 2));
+        batch.end();
         camera.update();
 
 
@@ -85,5 +115,51 @@ public class PlayState extends GameState {
     public void dispose() {
 
     }
+
+    public Body createBox(float x, float y, float width, float height, boolean isStatic) {
+        Body pBody;
+
+        // pBody properties
+        BodyDef bdef = new BodyDef();
+        if(isStatic)
+            bdef.type = BodyDef.BodyType.StaticBody;
+        else
+            bdef.type = BodyDef.BodyType.DynamicBody;
+        bdef.position.set(x / 70, y / 70);
+        bdef.fixedRotation = true;
+
+        // init pBody in world
+        pBody = world.createBody(bdef);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(width / 70, height / 70); // set from center, we want 32x32 box so we divide by 2
+
+        // add fixture to body and dispose
+        pBody.createFixture(shape, 1.0f);
+        shape.dispose();
+
+        return pBody;
+    }
+
+    public void inputUpdate(float dt) {
+        int horizontalForce = 0;
+
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            horizontalForce -= 1;
+            System.out.println("LEFT");
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            horizontalForce += 1;
+            System.out.println("RIGHT");
+        }
+        // only update once on key press
+        if(Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+            player.applyForceToCenter(new Vector2(0, 200f), false);
+            System.out.println("UP");
+        }
+
+        player.setLinearVelocity(horizontalForce * 5, player.getLinearVelocity().y);
+    }
+
 }
 
